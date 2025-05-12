f9cc7198e5c044ccc024e5e97af240f1af5c1cad
Oscar Limachi Tapia <oscarlimachitapia@gmail.com>
2025-05-12 15:02:14 +0200

    taskDao find by Id

--- app/src/main/java/com/example/tasklist/data/TaskDAO.kt
+++ app/src/main/java/com/example/tasklist/data/TaskDAO.kt
@@ -9,69 +9,91 @@ import kotlinx.coroutines.selects.select
 
 class TaskDAO(private val context: Context) {
     private lateinit var db: SQLiteDatabase
-    fun open(){
+    fun open() {
         db = DatabaseManager(context).writableDatabase
     }
-    fun close(){
+
+    fun close() {
         db.close()
     }
     //create actions
 
     //Insert
-    fun insert(task: Task){
-        open()
-        try {
-            val values = ContentValues().apply {
-                put(Task.COLUMN_NAME_TITLE,task.title)
-                put(Task.COLUMN_NAME_DONE,task.done)
-                put(Task.COLUMN_NAME_CATEGORY,task.category.id)
-            }
-            val newRowId = db.insert(Task.TABLE_NAME,null,values)
-            Log.i("DATABASE", "Inserted a task with id: $newRowId")
-        }catch (e: Exception){
-            e.printStackTrace()
-        }finally {
-            close()
-        }
-
-    }
-    //Update
-    fun update(task: Task){
+    fun insert(task: Task) {
         open()
         try {
             val values = ContentValues().apply {
                 put(Task.COLUMN_NAME_TITLE, task.title)
-                put(Task.COLUMN_NAME_DONE,task.done)
-                put(Task.COLUMN_NAME_CATEGORY,task.category.id)
+                put(Task.COLUMN_NAME_DONE, task.done)
+                put(Task.COLUMN_NAME_CATEGORY, task.category.id)
             }
-            //which row to update, based on the id
-            val selection= "${Task.COLUMN_NAME_ID} = ${task.id}"
-            val count = db.update(Task.TABLE_NAME,values,selection,null)
-            Log.i("DATABASE", "Update task with id: ${task.id}")
-        }catch (e: Exception){
-            e.printStackTrace()
-        }finally {
-            close()
-        }
-
-    }
-    //Delete
-    fun delete(task: Task){
-        open()
-        try {
-            val selection = "${Task.COLUMN_NAME_ID}=${task.id}"
-            val deleteRows = db.delete(Task.TABLE_NAME,selection,null)
-            Log.i("DATABASE", "Delete task with id: ${task.id}")
-        }catch (e: Exception){
+            val newRowId = db.insert(Task.TABLE_NAME, null, values)
+            Log.i("DATABASE", "Inserted a task with id: $newRowId")
+        } catch (e: Exception) {
             e.printStackTrace()
         } finally {
             close()
         }
 
     }
-    //Find by Id
-    fun findById(id:Long){
+
+    //Update
+    fun update(task: Task) {
         open()
+        try {
+            val values = ContentValues().apply {
+                put(Task.COLUMN_NAME_TITLE, task.title)
+                put(Task.COLUMN_NAME_DONE, task.done)
+                put(Task.COLUMN_NAME_CATEGORY, task.category.id)
+            }
+            //which row to update, based on the id
+            val selection = "${Task.COLUMN_NAME_ID} = ${task.id}"
+            val count = db.update(Task.TABLE_NAME, values, selection, null)
+            Log.i("DATABASE", "Update task with id: ${task.id}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
+    }
+
+    //Delete
+    fun delete(task: Task) {
+        open()
+        try {
+            val selection = "${Task.COLUMN_NAME_ID}=${task.id}"
+            val deleteRows = db.delete(Task.TABLE_NAME, selection, null)
+            Log.i("DATABASE", "Delete task with id: ${task.id}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
+    }
+
+    //Find by Id
+    fun findById(id: Long): Task? {
+        open()
+        val task: Task? = null
+        val projection = arrayOf(
+            Task.COLUMN_NAME_ID,
+            Task.COLUMN_NAME_TITLE,
+            Task.COLUMN_NAME_DONE,
+            Task.COLUMN_NAME_CATEGORY
+        )
+        val selection = "${Task.COLUMN_NAME_ID} = ${id}"
+        val cursor = db.query(
+            Task.TABLE_NAME,
+            projection,
+            selection,
+            null,
+            null,
+            null,
+            null
+        )
+        if (cursor.moveToNext(){
+
+            }
 
     }
 }
