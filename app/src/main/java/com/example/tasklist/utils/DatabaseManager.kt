043e8071bf3c78a2e75a9c9168dbb6e362347c5e
oscarAndroid <oscarlimachitapia@gmail.com>
2025-05-12 19:13:03 +0200

    empezamos con la vista de tareas

--- app/src/main/java/com/example/tasklist/utils/DatabaseManager.kt
+++ app/src/main/java/com/example/tasklist/utils/DatabaseManager.kt
@@ -8,7 +8,7 @@ import com.example.tasklist.data.Task
 
 class DatabaseManager(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null, DATABASE_VERSION) {
     companion object {
-        const val DATABASE_VERSION=1
+        const val DATABASE_VERSION=2
         const val DATABASE_NAME="toDoList.db"
         //Category
         private const val SQL_CREATE_CATEGORY =
